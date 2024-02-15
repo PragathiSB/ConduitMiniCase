ab3d6a788987be1e78a8ff46d0ee786dd728a35f
PragathiSB <pragathisayee@gmail.com>
2024-02-15 12:00:18 +0530

    commit 7

--- src/test/java/testScripts/CondiutArticleTest.java
+++ src/test/java/testScripts/CondiutArticleTest.java
@@ -123,7 +123,7 @@ public class CondiutArticleTest {
 	  @AfterMethod
 	  public void teardown(ITestResult result)
 	  {
-		  extentTest.assignAuthor("AutomationTester1-pragathi")
+		  extentTest.assignAuthor("AutomationTester-pragathi")
 		  .assignCategory("Regression Test")
 		  .assignDevice(System.getProperty("os.name"))
 		  .assignDevice(System.getProperty("os.version"));
